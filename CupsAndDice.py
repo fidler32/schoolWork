53e2e66f5967feac6dc874880e36cdd9758818cc
fidler32 <fidler3@gmail.com>
2020-02-10 17:50:13 -0700

    pretty done, need to add comments and help

--- CupsAndDice.py
+++ CupsAndDice.py
@@ -14,7 +14,6 @@ class SixSidedDie():
 
     def roll(self):
         self.faceValue = randint(1, self.n)
-        print(self.faceValue)
 
     def getFaceValue(self):
         return self.faceValue
@@ -68,7 +67,6 @@ class Cup():
             for dice in diceList:
                 dice.roll()
         self.setSum()
-        print(self.getSum())
 
     def getAllFaceValues(self):
         for key, diceList in self.diceDict.items():
@@ -84,11 +82,12 @@ class Cup():
 
 class DiceGame():
 
-    def __init__(self, balance):
+    def __init__(self, balance, name):
         self.balance = balance
         self.bet = 0
         self.goal = None
         self.cup = Cup(0, 0, 0)
+        self.name = name
 
     def setGoal(self):
         self.goal = randint(1, 100)
@@ -107,7 +106,7 @@ class DiceGame():
             return False
         self.bet = bet
         self.__deduct()
-        self.printBalance()
+        print("You have bet {}, balance is now {}".format(self.bet, self.balance))
         return True
 
     def __deduct(self):
@@ -117,18 +116,20 @@ class DiceGame():
         self.balance += self.bet * multiplier
 
     def __payout(self, rollTotal):
-        if rollTotal - self.goal < 0:
-            print("You busted and lost")
+        d = self.goal - rollTotal
+        print("You rolled {}".format(rollTotal))
+        if d < 0:
+            print("You busted and lost, you win nothing")
             return
-        if rollTotal - self.goal <= 3:
-            print("Within 3")
+        if d <= 3:
+            print("Within 3 ")
             self.__addWinnings(5)
             return
-        if rollTotal - self.goal <= 10:
+        if d <= 10:
             print("Within 10")
             self.__addWinnings(2)
             return
-        if rollTotal - self.goal == 0:
+        if d == 0:
             print("Winner Winner, Chicken Dinner")
             self.__addWinnings(10)
             return
@@ -136,6 +137,7 @@ class DiceGame():
             print("You lose")
 
     def gameRoll(self):
+        print("---SHAKE SHAKE AND ROLL---\n")
         self.cup.roll()
         self.__payout(self.cup.getSum())
         self.printBalance()
@@ -144,28 +146,107 @@ class DiceGame():
         return self.balance
 
     def printBalance(self):
-        print("Balance is now {}".format(self.getBalance()))
+        print("{}, Your balance is {}".format(self.name, self.getBalance()))
 
     def setCup(self, six, ten, twenty):
         self.cup = Cup(six, ten, twenty)
 
+    def getBet(self):
+        return self.bet
 
 
-def greet():
-    name = input("Hi welcome, what is your name?")
-    return name
+
+def sixSidedDie():
+    while (True):
+        try:
+            # split the input
+            d = int(input("How many six sided die would you like?\n"))
+            break
+
+        except ValueError:
+            print("Enter two integer values")
+
+    return d
+
+def tenSidedDie():
+    while (True):
+        try:
+            # split the input
+            d = int(input("How many ten sided die would you like?\n"))
+            break
+
+        except ValueError:
+            print("Enter two integer values")
+
+    return d
+
+def twentySidedDie():
+    while (True):
+        try:
+            # split the input
+            d = int(input("How many twenty sided die would you like?\n"))
+            break
+
+        except ValueError:
+            print("Enter two integer values")
+
+    return d
 
 def doYouWantToPlay():
-    answer = input("I've provided you with a balance of 100 dollars, would you like to play a game? Y or N?")
+    while(True):
+        answer = input("Would you like to play a game? y or n?\n")
+
+        if answer.lower() == "y":
+            return answer
+        if answer.lower() == "n":
+            print("Goodbye!")
+            exit()
+
+        print("Invalid input, please y for yes and n for no")
 
 
+def greetAndGetName():
+
+    name = input("Hi!! What is your name?\n")
+
+    return name
+
+
+def setUpDiceFromUser():
+
+    sixDie = sixSidedDie()
+    tenDie = tenSidedDie()
+    twentyDie = twentySidedDie()
+
+    return sixDie, tenDie, twentyDie
+
+def setBet(diceGame):
+    while (True):
+        try:
+            # split the input
+            bet = int(input("How much would you like to bet?\n"))
+            if diceGame.setBet(bet):
+                break
+            else:
+                raise ValueError
+        except ValueError:
+            print("Enter a valid integer value")
 
 def main():
-    diceGame = DiceGame(100)
-    diceGame.setCup(1, 1, 1)
-    diceGame.setGoal()
-    diceGame.setBet(20)
-    diceGame.gameRoll()
+    name = greetAndGetName()
+    diceGame = DiceGame(100, name)
+    diceGame.printBalance()
+
+    while 1:
+        doYouWantToPlay()
+        s, t, tw = setUpDiceFromUser()
+        diceGame.setCup(s, t, tw)
+        diceGame.setGoal()
+        setBet(diceGame)
+        print()
+        diceGame.gameRoll()
+
+
 
 
 if __name__ == '__main__':
